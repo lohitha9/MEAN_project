9266c4e7515aba2be0b710a6efa1ab1ff7ea524e
lohitha <34362198+lohitha9@users.noreply.github.com>
2018-01-05 15:15:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ var mongojs = require('mongojs');
 var db = mongojs('contactlist', ['contactlist']);
 var bodyParser = require('body-parser');
 
-// app.get('/', function (req, res) {
-//     res.send('Hello world from server.js');
-// });
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
@@ -17,23 +14,6 @@ app.get('/contactlist', function (req, res) {
         console.log(docs);
         res.json(docs);
     })
-    // person1 = {
-    //     name: 'Lohi',
-    //     email: 'lohi@xyz.com',
-    //     number: '(111) 111-1111'
-    // };
-    // person2 = {
-    //     name: 'Sandy',
-    //     email: 'Sandy@xyz.com',
-    //     number: '(222) 222-22225'
-    // };
-    // person3 = {
-    //     name: 'Pikachu',
-    //     email: 'Ash@xyz.com',
-    //     number: '(333) 333-3333'
-    // };
-    // var contactlist = [person1, person2, person3];
-    // res.json(contactlist);
 });
 app.post('/contactlist', function (req, res) {
     console.log(req.body);
@@ -49,7 +29,7 @@ app.delete('/contactlist/:id', function (req, res) {
         res.json(doc);
     })
 });
-// app.edit()
+
 app.get('/contactlist/:id', function (req, res) {
     var id = req.params.id;
     console.log(id);
